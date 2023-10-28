9fa5fd6f9ea344771e92a7eebfdd45df1fbfcfd3
yobett <yobett@hxr.com>
2023-10-28 13:12:23 +0800

    f

--- train_dreambooth.py
+++ train_dreambooth.py
@@ -135,7 +135,7 @@ def main(args):
         args.pretrained_model_name_or_path = pretrained_model_path
 
     accelerator_project_config = ProjectConfiguration(project_dir=args.output_dir,
-                                                      logging_dir=str(logging_dir))
+                                                      logging_dir=args.logging_dir)
 
     accelerator = Accelerator(
         gradient_accumulation_steps=args.gradient_accumulation_steps,
